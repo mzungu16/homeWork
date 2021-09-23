51487301a51d1d644c3666a876cd5506312726cf
smirnovGleb16 <gleb.s.smirnov@gmail.com>
2021-09-23 10:49:35 +0400

    Updated homeWork

--- src/main/java/lesson9/Lesson9.java
+++ src/main/java/lesson9/Lesson9.java
@@ -17,35 +17,46 @@ public class Lesson9 {
     static Scanner SCANNER = new Scanner(System.in);
     static Random RANDOM = new Random();
     static int SUMMER = 0;
+    static final int CONSTANT_SIZE = 4;
+    static String ELEMENT_TO_REMEMBER;
 
     public static void main(String[] args) {
         System.out.println("Enter array size- ");
         int inputSize = SCANNER.nextInt();
-        String[] strings = {"5", "7", "test", "45", "8", "6+8", "1", "34", "1", "3", "9"};
-        String[][] arrayToCheck = new String[inputSize][inputSize];
-        if (inputSize > 4) {
-            throw new MyArraySizeException("Array is not 4X4");
+        String[][] arrayToCheck = new String[CONSTANT_SIZE][CONSTANT_SIZE];
+        checkMethod(arrayToCheck, inputSize);
+    }
+
+    private static void checkMethod(String[][] arrayToCheck, int inputSize) {
+        try {
+            fillArrayWithStrings(arrayToCheck, inputSize);
+            System.out.println(fromStringToIntMethod(arrayToCheck, inputSize));
+        } catch (ArrayIndexOutOfBoundsException arrayIndexOutOfBoundsException) {
+            throw new MyArraySizeException("Array not 4X4");
+        } catch (NumberFormatException numberFormatException) {
+            throw new MyArrayDataException("Can't parseInt '" + ELEMENT_TO_REMEMBER + "' / " + +SUMMER + " - your total sum");
         }
+    }
+
+    private static int fromStringToIntMethod(String[][] arrayToCheck, int inputSize) {
+        int[][] newArrayOfInts = new int[CONSTANT_SIZE][CONSTANT_SIZE];
+        for (int i = 0; i < inputSize; i++) {
+            for (int j = 0; j < inputSize; j++) {
+                ELEMENT_TO_REMEMBER = arrayToCheck[i][j];
+                newArrayOfInts[i][j] = Integer.parseInt(arrayToCheck[i][j]);
+                SUMMER += newArrayOfInts[i][j];
+            }
+        }
+        return SUMMER;
+    }
+
+    private static void fillArrayWithStrings(String[][] arrayToCheck, int inputSize) {
+        String[] strings = {"5", "7", "test", "45", "8", "6+8", "1", "34", "1", "3", "9"};
         for (int i = 0; i < inputSize; i++) {
             for (int j = 0; j < inputSize; j++) {
                 arrayToCheck[i][j] = strings[RANDOM.nextInt(11)];
             }
         }
         System.out.println(Arrays.deepToString(arrayToCheck));
-        checkMethod(arrayToCheck);
-    }
-
-    private static void checkMethod(String[][] arrayToCheck) {
-        int[][] newArrayOfInts = new int[arrayToCheck[0].length][arrayToCheck.length];
-        for (int i = 0; i < arrayToCheck[0].length; i++) {
-            for (int j = 0; j < arrayToCheck.length; j++) {
-                try {
-                    newArrayOfInts[i][j] = Integer.parseInt(arrayToCheck[i][j]);
-                    SUMMER += newArrayOfInts[i][j];
-                } catch (NumberFormatException numberFormatException) {
-                    throw new MyArrayDataException("Can't parseInt '" + arrayToCheck[i][j] + "' " + "with indexes " + i + ":" + j + ",total sum = " + SUMMER);
-                }
-            }
-        }
     }
 }
