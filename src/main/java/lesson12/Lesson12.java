bf46049c99a4d8edd113815dca69bf5206444f97
smirnovGleb16 <gleb.s.smirnov@gmail.com>
2021-10-07 17:33:27 +0400

    homeWork

--- src/main/java/lesson12/Lesson12.java
+++ src/main/java/lesson12/Lesson12.java
@@ -12,13 +12,13 @@ public class Lesson12 {
         calculateValueWithThreads();
     }
     private static void calculateValueWithThreads() {
+        long timeOfBegin1 = System.currentTimeMillis();
         float[] myArray1 = new float[SIZE_OF_ARRAY];
         Arrays.fill(myArray1, 1f);
         float[] myArray2 = new float[SIZE_OF_ARRAY / 2];
         System.arraycopy(myArray1, 0, myArray2, 0, SIZE_OF_ARRAY / 2);
         float[] myArray3 = new float[SIZE_OF_ARRAY / 2];
         System.arraycopy(myArray1, SIZE_OF_ARRAY / 2, myArray3, 0, SIZE_OF_ARRAY / 2);
-        long timeOfBegin1 = System.currentTimeMillis();
         new Thread(() -> calculate(myArray2)).start();
         new Thread(() -> calculate(myArray3)).start();
         System.arraycopy(myArray2, 0, myArray1, 0, myArray2.length);
@@ -33,9 +33,9 @@ public class Lesson12 {
     }
 
     private static void calculateValueWithOutThreads() {
+        long timeOfBegin = System.currentTimeMillis();
         float[] myArray = new float[SIZE_OF_ARRAY];
         Arrays.fill(myArray, 1f);
-        long timeOfBegin = System.currentTimeMillis();
         for (int i = 0; i < SIZE_OF_ARRAY; i++) {
             myArray[i] = (float) (myArray[i] * Math.sin(0.2f + (i / 5)) * Math.cos(0.2f + (i / 5)) * Math.cos(0.4f + (i / 2)));
         }
