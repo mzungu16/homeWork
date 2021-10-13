02e092136c6dfe6561fd1cb16cee265023b5ba77
smirnovGleb16 <gleb.s.smirnov@gmail.com>
2021-10-13 07:26:59 +0400

    New commit

--- src/main/java/lesson13/Road.java
+++ src/main/java/lesson13/Road.java
@@ -4,22 +4,27 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Road extends Stage {
-    private static Map<String,Long> hashMap = new HashMap();
+    private static Map<String, Long> hashMap = new HashMap();
+    private static int COUNTER = 0;
 
     public Road(int length) {
         this.length = length;
         this.description = "Дорога " + length + " метров";
     }
 
-    public static Map getFinishMap() {
+    public static int getCOUNTER() {
+        return COUNTER;
+    }
+
+    public static Map<String, Long> getHashMap() {
         return hashMap;
     }
 
     public static void winner() {
         long min = 200000000000000L;
         String key = "";
-        for (Map.Entry<String,Long> entry : hashMap.entrySet()) {
-            if (min > entry.getValue()){
+        for (Map.Entry<String, Long> entry : hashMap.entrySet()) {
+            if (min > entry.getValue()) {
                 key = entry.getKey();
                 min = entry.getValue();
             }
@@ -35,6 +40,12 @@ public class Road extends Stage {
             System.out.println(c.getName() + " закончил этап: " + description);
             if (this.length == 40) {
                 hashMap.put(c.getName(), System.currentTimeMillis());
+                COUNTER++;
+            }
+            if (COUNTER == Lesson13.CARS_COUNT) {
+                Lesson13.threadEnd.start();
+                Thread.sleep(500);
+                Lesson13.threadPrint.start();
             }
         } catch (InterruptedException e) {
             e.printStackTrace();
